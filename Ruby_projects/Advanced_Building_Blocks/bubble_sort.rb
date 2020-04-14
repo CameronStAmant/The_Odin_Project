fe90aa8ba5e1547c4b689f366ebff1c96930daa9
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2020-04-14 21:01:34 +0000

    Update bubble_sort.rb

--- Ruby_projects/Advanced_Building_Blocks/bubble_sort.rb
+++ Ruby_projects/Advanced_Building_Blocks/bubble_sort.rb
@@ -1,22 +1,41 @@
 def bubble_sort(array)
-    i = 0
-    while array != array.sort
-        if array[i + 1].is_a? Integer
-            #print array
-            #puts
-            if array[i] > array[i + 1]
-                array[i], array[i + 1] = array[i + 1], array[i]
-                i += 1
-            else 
-                #print array
-                #puts
-                i += 1
-            end
-        else
-            i = 0
-        end
+  pass_throughs = 0
+  compare = 0
+  while pass_throughs < array.length
+    while compare < array.length - 1
+      if array[compare] > array[compare + 1]
+        array[compare], array[compare + 1] = array[compare + 1], array[compare]
+      end
+      compare += 1
     end
-    print array
+    compare = 0
+    pass_throughs += 1
+   end
+  array
 end
 
-bubble_sort([4,3,78,2,0,2])
+# Unhide the below puts to test the bubble_sort method.
+# puts bubble_sort([4,3,78,2,0,2])
+
+def bubble_sort_by(array)
+  pass_throughs = 0
+  compare = 0
+  while pass_throughs < array.length
+    while compare < array.length - 1
+      if yield(array[compare], array[compare + 1]) < 0
+        array[compare], array[compare + 1] = array[compare + 1], array[compare]
+      end
+      compare += 1
+    end
+    compare = 0
+    pass_throughs += 1
+  end
+  print array
+end
+
+bubble_sort_by = bubble_sort_by(["hi","hello","hey"]) do |left,right|
+    left.length - right.length
+    end
+
+# Unhide the below puts to test the bubble_sort_by method.
+# puts bubble_sort_by
