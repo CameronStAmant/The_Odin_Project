ccbeafa02c38a4a1a0b462e3f9a7f75568ab6296
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2020-04-16 12:53:16 +0000

    Update enumerable_methods.rb

--- Ruby_projects/Advanced_Building_Blocks/enumerable_methods.rb
+++ Ruby_projects/Advanced_Building_Blocks/enumerable_methods.rb
@@ -1,297 +1,197 @@
+# Unhide the commented out code, one line per run, to see it in action.
+
 module Enumerable
-    def my_each
-        if block_given?
-            if self.is_a?(Array)
-                self.length.times { |i| yield(self[i]) }
-                self
-            else
-                self.length.times { |i| yield(self.to_a.at(i)) }
-                self
-            end
-        else
-            "#<Enumerator: #{self}:my_each"
-        end
+  def my_each
+    i = 0
+    while i < self.length
+      yield(self[i])
+      i += 1
     end
+  end
 
-=begin
-    QA  
-        arrays:
-            arr = [2,3,4,5]
-            arr.my_each { |a| p a }
+# [1, 2, 3, 4, 5].my_each { |a| puts a + 1}
+# [1, 2, 3, 4, 5].each { |a| puts a + 1}
 
-        hashes:
-            hashh = { "two" => "2", "three" => "3" }
-            hash.my_each { |a| p a }
-=end
-
-    def my_each_with_index
-        if block_given?
-            if self.is_a?(Array)
-                self.length.times { |i| yield(self[i], i) }
-                self
-            else 
-                self.length.times { |i| yield(self.keys[i], self.values[i], i) }
-                self
-            end
-        else
-            "#<Enumerator: #{self}:my_each_with_index>"
-        end
+  def my_each_with_index
+    i = 0
+    while i < self.length
+      yield(self[i], i)
+      i += 1
     end
+  end
 
-=begin
-    QA
-        arrays:
-            arr = [2,3,4,5]
-            arr.my_each_with_index { |value, index| puts "#{index}: #{value}" }
+# [1, 2, 3, 4, 5].my_each_with_index { |a, index| puts "#{index}: #{a + 1}"}
+# [1, 2, 3, 4, 5].each_with_index { |a, index| puts "#{index}: #{a + 1}"}
 
-        hashes:
-            hashh = { "two" => "2", "three" => "3" }
-            hashh.my_each_with_index { |key, value, index| puts "[\"#{key}\", \"#{value}\", #{index}]" }
-=end
-
-    def my_select
-        if block_given?
-        ary = []
-        has = {}
-            if self.is_a?(Array)
-                self.my_each { |n| yield(n) == true ? ary.push(n) : n }
-                ary
-            else
-                self.my_each { |k, v| yield(k, v) == true ? has[k] = v : nil }
-                has
-            end
-        else
-            "#<Enumerator: #{self}:my_select"
-        end
+  def my_select
+    i = 0
+    array = []
+    while i < self.length
+      if yield(self[i]) == true
+        array << self[i]
+      end
+      i += 1
     end
+    array
+  end
 
-=begin
-    QA
-        arrays:
-            arr = [2,3,4,5]
-            arr.my_select { |i| i % 2 == 0 }
+# puts [1,2,3,4,5].my_select { |num|  num.even?  }
+# puts [1,2,3,4,5].select { |num|  num.even?  }
 
-        hashes:
-            hashh = { "two" => 2, "six" => 6, "four" => 4 }
-            hashh.my_select { |k, v| v > 3 }
-=end
-
-    def my_all?(type = nil)
-        ary = []
-        has = {}
-        if block_given?
-            if self.is_a?(Array)
-                self.my_each { |n| yield(n) == true ? ary.push(true) : false }
-                self.length == ary.length ? (puts "true") : (puts "false")
-                ary
-            else
-                self.my_each { |k, v| yield(k, v) == true ? has[k] = v : (puts "hash") }
-                has
-            end
-        
-        elsif type != nil
-            self.my_each { |n| n.is_a?(type) ? ary.push(true) : false }
-            self.length == ary.length ? (puts "true") : (puts "false")
-        
-        else
-            self.my_each { |n| n != false && n != nil ? ary.push(true) : false }
-            self.length == ary.length ? (puts "true") : (puts "false")
-        end
+  def my_all?
+    i = 0
+    while i < self.length
+      if yield(self[i]) == false
+        i = self.length + 1
+        break
+      end
+      i += 1
     end
-
-=begin
-    QA
-        array:
-            arr = [2,3,4,5]
-            arr.my_all? { |i| i >= 3 }
-            arr.my_all? { |i| i >= 2 }
-            arr.my_all?(Numeric)
-            arr1 = [2,3,"hi"]
-            arr1.my_all?(Numeric)
-            arr2 = [nil, 2]
-=end
-
-    def my_any?(type = nil)
-        ary = []
-        has = {}
-        if block_given?
-            if self.is_a?(Array)
-                self.my_each { |n| yield(n) == true ? ary.push(true) : false }
-                ary.length >= 1 ? (puts "true") : (puts "false")
-                ary
-            else
-                self.my_each { |k, v| yield(k, v) == true ? has[k] = v : false }
-                has.length >= 1 ? (puts "true") : (puts "false")
-                has
-            end
-        
-        elsif type != nil
-            self.my_each { |n| n.is_a?(type) ? ary.push(true) : false }
-            self.length == ary.length ? (puts "true") : (puts "false")
-        
-        else
-            self.my_each { |n| n != false && n != nil ? ary.push(true) : false }
-            ary.length >= 1 ? (puts "true") : (puts "false")
-        end
+    if i == self.length
+      puts "true"
+    else
+      puts "false"
     end
+  end
+  
+# puts %w[ant bear cat].my_all? { |word| word.length >= 3 }
+# puts  %w[ant bear cat].my_all? { |word| word.length >= 4 }
+# puts  %w[ant bear cat].all? { |word| word.length >= 3 }
+# puts  %w[ant bear cat].all? { |word| word.length >= 4 }
 
-=begin
-    QA
-        Array:
-            arr = [2,3,4,5]
-            arr1 = [2,3,"hi"]
-            arr2 = [nil, 2]
-            arr1.my_any? { |i| i.is_a?(Numeric) }
-            arr2.my_any?
-            arr2.my_any?(Numeric)
-        Hash:
-            hashh = { "two" => 2, "six" => 6, "four" => 4 }
-            hashh.my_any?
-            hashh.my_any? { |k, v| k.is_a?(String) }
-            hashh.my_any? { |k, v| k.is_a?(Numeric) }
-=end
 
-    def my_none?(type = nil)
-        ary = []
-        has = {}
-        if block_given?
-            if self.is_a?(Array)
-                self.my_each { |n| yield(n) == true ? ary.push(true) : false }
-                ary.length >= 1 ? (puts "false") : (puts "true")
-                ary
-            else
-                self.my_each { |k, v| yield(k, v) == true ? has[k] = v : false }
-                has.length >= 1 ? (puts "false") : (puts "true")
-                has
-            end
-        
-        elsif type != nil
-            self.my_each { |n| n.is_a?(type) ? ary.push(true) : false }
-            self.length == ary.length ? (puts "false") : (puts "true")
-        
-        else
-            self.my_each { |n| n != false && n != nil ? ary.push(true) : false }
-            ary == nil ? (puts "true") : (puts "false")
-            ary
+  def my_any?(type = nil)
+    i = 0
+    while i < self.length
+      if type == nil
+        if yield(self[i]) == true
+          i = self.length + 1
+          break
         end
+        i += 1
+      else
+        self.my_each { |a| a.is_a?(Integer) ? i = self.length + 1 : i += 1}
+      end
     end
-
-=begin
-    QA
-        Array:
-            arr = [2,3,4,5]
-            arr1 = [2,3,"hi"]
-            arr2 = [nil, 2]
-            arr3 = [nil, false]
-            arr1.my_none? { |i| i.is_a?(Numeric) }
-            arr2.my_none?
-            arr2.my_none?(Numeric)
-        Hash:
-            hashh = { "two" => 2, "six" => 6, "four" => 4 }
-            hash1 = {}
-            hashh.my_none?
-            hashh.my_none? { |k, v| k.is_a?(String) }
-            hashh.my_none? { |k, v| k.is_a?(Numeric) }
-=end
-
-    def my_count(specific = nil)
-        i = 0
-        if block_given?
-            self.my_each { |n| yield(n) == true ? i += 1 : i += 0 }
-            i
-        elsif specific != nil
-            self.my_each { |n| n == specific ? i += 1 : i += 0 }
-            i
-        else
-            self.length
-        end
+    if i > self.length
+      puts "true"
+    else
+      puts "false"
     end
+  end
+# puts %w[ant bear cat].my_any? { |word| word.length >= 4 } #=> true
+# puts [nil, true, "a"].my_any?(Integer)
+# puts %w[ant bear cat].any? { |word| word.length >= 4 } #=> true
+# puts [nil, true, "a"].my_any?(Integer) #=> false
 
-=begin
-    QA
-        Array:
-            arr = [2,3,4,2,5,2,2]
-            arr.my_count
-            arr.my_count(2)
-            arr.my_count { |x| x % 2 == 0 }
 
-=end
 
-    def my_map(&pro)
-        ary = []
-        if block_given?
-            self.my_each { |n| ary << yield(n) }
-            return ary
-        else
-            "#<Enumerator: #{self}:my_map"
-        end
+  def my_none?
+    i = 0
+    while i < self.length
+      if yield(self[i]) == true
+        i = self.length + 1
+        break
+      end
+      i += 1
     end
-
-=begin
-    QA
-        Array:
-            arr = [2,3,4,2,5,2,2]
-            arr.my_map { |i| i - 10 }
-        Proc:
-            my_proc = Proc.new { |x| x * x}
-            arr.my_map(&my_proc)
-=end
-
-    def my_inject(val = nil, sym = nil)
-        result = self[0]
-        if val.is_a?(String) || val.is_a?(Symbol)
-            sym = val
-            val = nil
-        end
-        if val.is_a?(Integer)
-            result = val
-        end
-        if sym.nil?
-            if block_given?
-                if val == nil
-                    drop(1).my_each { |i| result = yield(result, i) }
-                else
-                    result = val
-                    my_each { |i| result = yield(result, i) }
-                end
-            end
-        else
-            if sym == :* || sym == :/
-                if val == nil
-                    drop(1).my_each { |i| result = result.send(sym, i) }
-                else
-                    result = val
-                    my_each { |i| result = result.send(sym, i) }     
-                end
-            else
-                if val != nil
-                    my_each { |i| result = result.send(sym, i) }
-                else
-                    drop(1).my_each { |i| result = result.send(sym, i) }
-                end
-            end
-        end
-        return result
+    if i == self.length
+      puts "true"
+    else
+      puts "false"
     end
+  end
 
-=begin
-    QA
-        Array:
-        arr = [16,2]
-        arr.my_inject(:+)
-        arr.my_inject(64, :+)
-        arr.my_inject { |sum, n| sum + n }  
-        arr.my_inject(64) { |sum, n| sum + n }
+# puts %w{ant bear cat}.my_none? { |word| word.length == 5 } #=> true
+# puts %w{ant bear cat}.my_none? { |word| word.length >= 4 } #=> false
+# puts %w{ant bear cat}.none? { |word| word.length == 5 } #=> true
+# puts %w{ant bear cat}.none? { |word| word.length >= 4 } #=> false
 
-=end
 
-    def multiply_els
-        my_inject(:*)
+#my_count
+  def my_count(count = nil)
+    i = 0
+    tally = 0
+    while i < self.length
+      if block_given?
+        if yield(self[i]) == true
+          tally += 1
+        end
+      elsif count != nil
+        if self[i] == count
+          tally += 1
+        end
+      else
+        tally = self.length
+      end
+      i += 1
     end
+    puts tally
+  end
 
-=begin 
-    QA
-        [2,4,5].multiply_els
-=end
+
+# puts [1, 2, 4, 2].my_count               #=> 4
+# puts [1, 2, 4, 2].my_count(2)            #=> 2
+# puts [1, 2, 4, 2].my_count{ |x| x%2==0 } #=> 3
+# puts [1, 2, 4, 2].count               #=> 4
+# puts [1, 2, 4, 2].count(2)            #=> 2
+# puts [1, 2, 4, 2].count{ |x| x%2==0 } #=> 3
+
+
+
+  def my_map(myproc = nil)
+    array = self.to_a
+    array1 = []
+    array.my_each { |a| array1 << (myproc ? myproc.call(a) : yield(a)) }
+    print array1
+  end
+
+# print (1..4).my_map { |i| i*i }      #=> [1, 4, 9, 16]
+# print (1..4).my_map { "cat"  }   #=> ["cat", "cat", "cat", "cat"]
+# puts (1..4).map { |i| i*i }      #=> [1, 4, 9, 16]
+# puts (1..4).map { "cat"  }   #=> ["cat", "cat", "cat", "cat"]
+
+# Proc
+# my_proc = Proc.new { |x| x ** x}
+# (1..4).my_map(&my_proc)
+
+
+
+  def my_inject(memo = nil, element = nil)
+    array = self.to_a
+    result = 0
+    if block_given?
+      array.my_each { |b| result = yield(result, b) }
+    end
+    if memo == :+
+      array.my_each { |b| result += b }
+    elsif memo == :-
+      array.my_each { |b| result -= b }
+    end
+    if memo.is_a?(Integer)
+      result += memo
+      if element == :*
+        array.my_each { |b| result *= b }
+      elsif element == :/
+        array.my_each { |b| result /= b }
+      end
+    end
+    print result
+  end
+
+# puts (5..10).my_inject(:+) #=> 45
+# puts (5..10).my_inject(1, :*) #=> 151200
+# puts (5..10).my_inject { |sum, n| sum + n } #=> 45
+
+# puts (5..10).inject(:+) #=> 45
+# puts (5..10).inject(1, :*) #=> 151200
+# puts (5..10).inject { |sum, n| sum + n } #=> 45
+
+
+  def multiply_els(*ary)
+    puts my_inject(1, :*)
+  end
+
+# [2,4,5].multiply_els #=> 40
 
 end
