a18f4e3e840868e07a8a6897be58b13f195574ef
Cameron St. Amant <cameronstamant@protonmail.com>
2020-07-03 15:45:47 -0600

    Fixed small changes.

--- Ruby_projects/Chess/lib/chess.rb
+++ Ruby_projects/Chess/lib/chess.rb
@@ -663,6 +663,6 @@ class Game
   end
 end
 
-## While the below is commented out, the rspecs will pass much quicker. Uncomment the below out to play a game.
+## While the below is commented out, the rspecs will pass much quicker. Uncomment the below to play a game.
 # game = Game.new
 # game.ui
